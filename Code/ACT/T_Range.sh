4c14cb765f3fd75c3ba892e423352b5a2feac582
Yifu Chen <rechen33@users.noreply.github.com>
2025-06-13 15:50:50 -0600

    add diff PWV to diff T

--- Code/ACT/T_Range.sh
+++ Code/ACT/T_Range.sh
@@ -7,16 +7,23 @@ rm -f diff_Tbase_inp.err
 # float sequence
 for Tchage in $(awk '
         BEGIN {
-            for (p = -20.0; p <= +20.1; p += +10.0)
+            for (p = -20.0; p <= +20.1; p += +20.0)
                 printf("%+.1f\n", p)
             }
     '); do
 
 echo "Tchage = $Tchage"
 
-printf "%+.1f 0\n" $Tchage >> diff_Tbase_inp.txt
+printf "%+.1f 1 0\n" $Tchage >> diff_Tbase_inp.txt
 
-am ACT_DJF_5_T_R_$Tchage.amc \
+am ACT_DJF_5_T_R_1pwv_$Tchage.amc \
     >> diff_Tbase_inp.txt 2>>diff_Tbase_inp.err
+
+printf "%+.1f 10 0\n" $Tchage >> diff_Tbase_inp.txt
+
+am ACT_DJF_5_T_R_10pwv_$Tchage.amc \
+    >> diff_Tbase_inp.txt 2>>diff_Tbase_inp.err
+
+#break
 
 done
