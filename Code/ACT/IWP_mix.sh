51679f4b347e247b757ed5d81267423757af0845
Yifu Chen <rechen33@users.noreply.github.com>
2025-06-18 15:42:00 -0600

    Edit plots & working on quasar for my code

--- Code/ACT/IWP_mix.sh
+++ Code/ACT/IWP_mix.sh
@@ -7,7 +7,9 @@ rm -f diff_IWP.err
 # float sequence
 for Percent in $(awk '
         BEGIN {
-            for (p = 0.1; p <= 0.91; p += 0.4)
+            for (p = 0.1; p <= 0.6; p += 0.4)
+                printf("%.1f\n", p)
+            for (p = 1.0; p < 1.1; p += 0.1)
                 printf("%.1f\n", p)
             }
     '); do
@@ -18,12 +20,6 @@ printf "%.1f 1 0\n" $Percent >> diff_IWP.txt
 
 am IWP/ACT_DJF_5_1pwv_iwpmix$Percent.amc \
     >> diff_IWP.txt 2>>diff_IWP.err
-
-# printf "%.1f 5 0\n" $Percent >> diff_IWP.txt
-
-# am IWP/ACT_DJF_5_5pwv_iwpmix$Percent.amc \
-#     >> diff_IWP.txt 2>>diff_IWP.err
-
 
 printf "%.1f 10 0\n" $Percent >> diff_IWP.txt
 
