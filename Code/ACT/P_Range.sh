9a244d73a3efa9c0d8a356a4287be95e98492d86
Yifu Chen <rechen33@users.noreply.github.com>
2025-06-24 15:26:14 -0600

    More P range

--- Code/ACT/P_Range.sh
+++ Code/ACT/P_Range.sh
@@ -7,27 +7,27 @@ rm -f diff_Pbase.err
 # float sequence
 for Pchage in $(awk '
         BEGIN {
-            for (p = 0.8; p <= 1.3; p += 0.2)
+            for (p = 0.1; p <= 2.0; p += 0.3)
                 printf("%.1f\n", p)
             }
     '); do
 
 echo "Pchage = $Pchage"
 
-printf "%.1f 1.0 0\n" $Pchage >> diff_Pbase.txt
+# printf "%.1f 1.0 0\n" $Pchage >> diff_Pbase.txt
 
-am Pmode/ACT_DJF_5_1.0pwv_P$Pchage.amc \
-    >> diff_Pbase.txt 2>>diff_Pbase.err
+# am Pmode/ACT_DJF_5_1.0pwv_P$Pchage.amc \
+#     >> diff_Pbase.txt 2>>diff_Pbase.err
 
-printf "%.1f 5.5 0\n" $Pchage >> diff_Pbase.txt
+# printf "%.1f 5.5 0\n" $Pchage >> diff_Pbase.txt
 
-am Pmode/ACT_DJF_5_5.5pwv_P$Pchage.amc \
-    >> diff_Pbase.txt 2>>diff_Pbase.err
+# am Pmode/ACT_DJF_5_5.5pwv_P$Pchage.amc \
+#     >> diff_Pbase.txt 2>>diff_Pbase.err
 
 
-printf "%.1f 10.0 0\n" $Pchage >> diff_Pbase.txt
+printf "%.1f 10 0\n" $Pchage >> diff_Pbase.txt
 
-am Pmode/ACT_DJF_5_10.0pwv_P$Pchage.amc \
+am Pmode/ACT_DJF_5_10pwv_P$Pchage.amc \
     >> diff_Pbase.txt 2>>diff_Pbase.err
 
 #break
