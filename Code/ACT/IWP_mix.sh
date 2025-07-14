d0c444d278a8d7b20968abaf10cce73a5e532399
Yifu Chen <rechen33@users.noreply.github.com>
2025-07-14 16:41:28 -0600

    test and done the amwrap for quasar

--- Code/ACT/IWP_mix.sh
+++ Code/ACT/IWP_mix.sh
@@ -1,30 +1,32 @@
 #use Script to show the diff PWV with diff mixing ratio of IWP
 
-rm -f diff_IWP_0.1-1_full.txt
-rm -f diff_IWP_0.1-1_full.err
+rm -f diff_IWP10test.txt
+rm -f diff_IWP10test.err
 
 #!/bin/bash
 # float sequence
 for Percent in $(awk '
         BEGIN {
-            for (p = 0.1; p <= 0.3; p += 0.4)
+            for (p = 0.3; p <= 0.91; p += 0.3)
                 printf("%.1f\n", p)
-            for (p = 1.0; p < 1.1; p += 0.1)
-                printf("%.1f\n", p)
+            
             }
     '); do
 
+#for (p = 1.0; p < 1.1; p += 0.1)
+#                printf("%.1f\n", p)
+
 echo "Percent of IWP = $Percent"
 
-printf "%.1f 0.1 0\n" $Percent >> diff_IWP_0.1-1_full.txt
+# printf "%.1f 0.1 0\n" $Percent >> diff_IWP_0.1-1_full.txt
 
-am IWP/ACT_DJF_5_0.1pwv_iwpmix$Percent.amc \
-    >> diff_IWP_0.1-1_full.txt 2>>diff_IWP_0.1-1_full.err
+# am IWP/ACT_DJF_5_0.1pwv_iwpmix$Percent.amc \
+#     >> diff_IWP_0.1-1_full.txt 2>>diff_IWP_0.1-1_full.err
 
-printf "%.1f 1 0\n" $Percent >> diff_IWP_0.1-1_full.txt
+printf "%.1f 10 0\n" $Percent >> diff_IWP10test.txt
 
-am IWP/ACT_DJF_5_1pwv_iwpmix$Percent.amc \
-    >> diff_IWP_0.1-1_full.txt 2>>diff_IWP_0.1-1_full.err
+am IWP/ACT_DJF_5_10pwv_iwpmix$Percent.amc \
+    >> diff_IWP10test.txt 2>>diff_IWP10test.err
 
 
 #break
