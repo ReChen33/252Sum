035a1d821cae3b51b75292552b2ad455db8ba85f
Yifu Chen <rechen33@users.noreply.github.com>
2025-07-17 16:47:49 -0600

    py file do the slow fit

---
+++ Code/Sofia/scanAz_slow_2fit_pwv.py
@@ -0,0 +1,274 @@
+"""
+25/07/17
+Ver 1.1
+Yifu
+
+Purpose:
+This code should show the PWV for diff time measurement for file 20250101_010135_scanAz_slow.txt
+Use the am fit by SPole_annual_50.amc(included the fit columns)
+
+"""
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from typing import Dict
+from time import perf_counter
+
+def remove_old_files():
+    """"
+    purpose:
+    remove old .dat, .dat.amc, .dat.ams, and .dat.amr files in the specified path to save space
+    """
+    import glob
+
+    try:
+        for file in glob.glob(f'{path_save}*.dat'):
+            #print(f"Removing file: {file}")
+            os.remove(file)
+        for file in glob.glob(f'{path_save}*.dat.amc'):
+            #print(f"Removing file: {file}")
+            os.remove(file)
+        for file in glob.glob(f'{path_save}*.dat.ams'):
+            #print(f"Removing file: {file}")
+            os.remove(file)
+        for file in glob.glob(f'{path_save}*.dat.amr'):
+            #print(f"Removing file: {file}")
+            os.remove(file)
+    except Exception as e:
+        print(f"Error removing files: {e}")
+
+
+
+def dat2Am(am_temp_inp = "SPole_annual_50.amc", zen_ang = 45, dat_fn_inp = "test.dat", path = None):
+    """
+    purpose:
+    use the am fits(am manual Ch7) to create amc;ams;amr files 
+    based on the {am_template}, {zen_ang}, and {dat_filename}   
+
+    input:
+        am_template: str, the am template filename
+        zen_ang: float, the zenith angle
+        dat_filename: str, the dat filename
+
+    output:
+        None, 
+        but creates {dat_filename}.amc; {dat_filename}.ams; {dat_filename}.amr files
+    """
+
+    os.system(f"am {am_temp_inp} {zen_ang} {dat_fn_inp}")
+    #print(f"{dat_fn_inp} created using {am_temp_inp} at zenith angle {zen_ang} degrees.")
+
+    #read the PWV from the new amc(index must read from new amc)
+    file_variable = open(dat_fn_inp+'.amc')
+    all_lines_variable = file_variable.readlines()
+    file_variable.close()
+
+    nscale_line=all_lines_variable[45]
+    nscale_com = nscale_line.split(' ')[0]  #get the nscale comment
+    nscale_com += "_" + nscale_line.split(' ')[1]
+    nscale_com += "_" + nscale_line.split(' ')[2]
+    #print('nscale_com=', nscale_com)
+
+    nscale_value = nscale_line.split(' ')[3].strip()  #get the nscale value
+    #print('nscale_value=', nscale_value)
+
+    std_line=all_lines_variable[10]
+    std_com = std_line.split(' ')[1]  #get the std comment
+    std_com += "_" + std_line.split(' ')[2]
+    std_com += "_" + std_line.split(' ')[3]
+    std_com += "_" + std_line.split(' ')[4]
+    
+    std_value = std_line.split(' ')[5].strip()  
+    std_unit = std_line.split(' ')[6].strip()  
+    #print('std_line=', std_line)
+
+    # pwv_line_meso=all_lines_variable[484]
+    # pwv_line_strato=all_lines_variable[491]
+    # pwv_line_tropo=all_lines_variable[498]
+    pwv_line_total=all_lines_variable[505]
+
+    # print('pwv_line_meso=', pwv_line_meso)
+    # print('pwv_line_strato=', pwv_line_strato)
+    # print('pwv_line_tropo=', pwv_line_tropo)
+    # print('pwv_line_total=', pwv_line_total)
+
+    #get the time from the dat_fn_inp
+    time = dat_fn_inp
+    if path is not None:
+        time = time.split('/')[-1] #except "~/results/20250101_010135_scanAz_slow.dat"
+    time = time.split('_')[0]  #example: "20250101T010136.531267_scanAz_slow.dat"
+
+    pwv_tot_zen = pwv_line_total.split('(')[1].strip() #remove the (
+    pwv_tot_zen = pwv_tot_zen.replace(')', '').strip()  #remove the )
+
+    pwv_tot_LoS = pwv_line_total.split('(')[2].strip() #remove the (
+    pwv_tot_LoS = pwv_tot_LoS.replace(')', '').strip()  #remove the )
+
+    #save the time and pwv values to a file
+    if not os.path.exists('time_pwv.csv'):
+        with open('time_pwv.csv', 'w') as pwv_file:
+            pwv_file.write(f"TIME,PWV TOTAL zenith,PWV TOTAL line-of-sight,zenith angle,{std_com},{nscale_com}\n")
+            pwv_file.write(f"{time},{pwv_tot_zen},{pwv_tot_LoS},{zen_ang},{std_value} {std_unit},{nscale_value}\n")
+        pwv_file.close()
+    else:
+        with open('time_pwv.csv', 'a') as pwv_file:
+            pwv_file.write(f"{time},{pwv_tot_zen},{pwv_tot_LoS},{zen_ang},{std_value} {std_unit},{nscale_value}\n")
+        pwv_file.close()
+    
+    
+
+
+def slow2Dat(path = None, scanAz_slow_filename = "20250101_010135_scanAz_slow.txt"):
+    """
+    purpose:
+    read the *_scanAz_slow.txt file and write a dat file
+
+    file format:
+    from *_scanAz_slow.txt we need: TIME(0); TSRC0(19); TSRC1(20); TSRC2(21); TSRC3(22); EL(23); AZ(24)
+    into dat:(for each col. which means diff channel) fre; bandwith; Temp
+
+    input:
+        scanAz_slow_filename: str, the filename of the scanAz_slow file, 
+            default is "20250101_010135_scanAz_slow.txt"
+
+    
+    """
+    read_time_start = perf_counter()  # start the timer for reading the file
+
+    if path is not None:
+        scanAz_slow_filename = os.path.join(path, scanAz_slow_filename)
+    
+    #use a Dict to save the data
+    #Dict = ["TIME":["TSRC0":value, "TSRC1":value, "TSRC2":value, "TSRC3":value, "EL":value, "AZ":value]]
+    out_dict: Dict[str, Dict[str,float]] = {}
+
+    #read the scanAz_slow file
+    with open(scanAz_slow_filename, 'r') as f:
+        for line in f:
+            if not (line.startswith('#') or line.startswith('TIME')):  
+                # skip comment lines & col names in scan_Az files
+                parts = line.split()
+
+                time = parts[0]  # TIME is the first part
+                #need rewrite time to let {time} able to be used as filename
+                time = time.replace(':', '').replace('-', '').replace(' ', '_')  # format TIME for filename
+                
+                out_dict[time] = {
+                    "TSRC0": float(parts[19]),
+                    "TSRC1": float(parts[20]),
+                    "TSRC2": float(parts[21]),
+                    "TSRC3": float(parts[22]),
+                    "EL": float(parts[23]),
+                    "AZ": float(parts[24])
+                }
+    f.close()
+    read_time_end = perf_counter()  # end the timer for reading the file
+    
+    with open('time_take.csv', 'w') as t:
+        t.write("TIME,take TIME (seconds)\n")
+        t.write(f"read {scanAz_slow_filename},{read_time_end - read_time_start:.4f}\n")
+
+    print(out_dict)  # print the dictionary to check the data
+    
+    num4Time = 0 #clear see the order
+
+    #A for loop to create the dat files based on diff TIME
+    #take the values for 10 sec(10 columns) from the Dict
+
+    for i in range(0, len(out_dict), 10):  # process 10 entries at a time
+        time, obs_values = list(out_dict.items())[i]  # get the current time and its values
+        
+        print(f"Processing TIME: {time}")
+        
+        start = perf_counter()  # start the timer
+
+        #write the dat filename based on the time
+        dat_filename = f"{time}_scanAz_slow.dat"
+
+        if path is not None:
+            dat_filename = os.path.join(path, dat_filename)
+
+        
+        
+        T0 = obs_values["TSRC0"]
+        T1 = obs_values["TSRC1"]
+        T2 = obs_values["TSRC2"]
+        T3 = obs_values["TSRC3"]
+        EL = obs_values["EL"] 
+        #AZ = obs_values["AZ"]
+
+        with open(dat_filename,'w+') as f:
+            #print('writing on '+ dat_filename)
+            f.write('1.25 1.50 {0}\n'.format(T0))
+            f.write('3.25 2.50 {0}\n'.format(T1))
+            f.write('5.5 2.00 {0}\n'.format(T2))
+            f.write('7.25 1.50 {0}'.format(T3))
+            #print(f'{time} dat writing done.')
+        f.close() 
+
+        zen = 90 - EL  # calculate zenith angle to elevation
+        am_temp = "SPole_annual_50.amc"
+        if path is not None:
+            am_temp = os.path.join(path, am_temp)
+
+        #call the dat2Am function to create amc;ams;amr files
+        dat2Am(am_temp_inp = am_temp, dat_fn_inp=dat_filename, zen_ang=zen, path=path)
+
+        remove_old_files() #to save space for my laptop
+
+        end = perf_counter()
+
+        #save the time taken to csv file        
+        with open('time_take.csv', 'a') as t:
+            t.write(f"{time},{end - start:.4f}\n")
+        t.close()
+
+        num4Time += 1
+
+        print(f"num of {num4Time} dat files done")
+
+        if num4Time >= 1:
+            #continue
+            break  # for testing, remove this line to process all times
+        
+        
+        
+
+
+try:
+    os.remove('time_take.csv')
+    os.remove('time_pwv.csv')
+    print("Old csv files removed.")
+except Exception as e:
+    print(e)
+
+path_save = "results/"
+
+slow2Dat(path = path_save, scanAz_slow_filename = "20250101_010135_scanAz_slow.txt")
+
+
+
+def sum_time_take():
+    """
+    purpose:
+    sum the time taken for each TIME in the time_take.csv file
+    """
+
+    total_time = 0
+
+    with open('time_take.csv', 'r') as t:
+        next(t)  # skip header
+        for line in t:
+            parts = line.split(',')
+            if len(parts) == 2:
+                total_time += float(parts[1])
+    t.close()
+
+    with open('time_take.csv', 'a') as t:
+        t.write(f"\nTotal time taken: {total_time:.4f} seconds\n")
+    t.close()
+
+    print(f"Total time taken: {total_time:.4f} seconds")
+
+sum_time_take()  # sum the time taken for each TIME
