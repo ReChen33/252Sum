1205e2229953194eb2b1680cece3b20fcbaaab82
Yifu Chen <rechen33@users.noreply.github.com>
2025-12-17 16:30:40 -0700

    amVSballoon goes through 2024

--- Code/Ballon/SkewT/amVSBalloon.py
+++ Code/Ballon/SkewT/amVSBalloon.py
@@ -13,6 +13,7 @@ from siphon.simplewebservice.wyoming import WyomingUpperAir
 
 import os
 import glob
+import time
 
 data_list = []
 
@@ -20,54 +21,69 @@ base_dir = os.path.dirname(os.path.abspath(__file__))
 if not os.path.exists(os.path.join(base_dir, 'fig1')):
     os.makedirs(os.path.join(base_dir, 'fig1'))
 
-Per_list = []
-Tem_list = []
-vmr_list = []
-
 amc_list = glob.glob(f'{base_dir}/*.dat.amc') 
 amc_list.sort()
 #print(amc_list)
 
 data_list = []
-
-for days in range(1,32):
-    # data00 = datetime(2024, 1, days, 0)
-    data12 = datetime(2024, 1, days, 12) 
-    # data_list.append(data00)
-    data_list.append(data12)
-
+for month in range(1,13):
+    for days in range(1,32):
+        try:
+            data12 = datetime(2024, month, days, 12)
+            data_list.append(data12)
+        except ValueError:
+            print(f'Invalid date {month}/{days}')
+            continue
+        
 data_list.sort()
 station = '89009'
-
 num_date = 0
 num_file_n = 0
 
+
 while num_date < len(data_list) and num_file_n < len(amc_list):
-    
+    #print(f'Number of am data: {len(data_list)}')
     date = data_list[num_date]
     file_path_n = amc_list[num_file_n]
     file_n = os.path.basename(file_path_n)
     getTime = file_n.split('S')[0]+file_n.split('S')[1].split('.')[0]
     amDate = datetime.strptime(getTime, '%Y%m%d%H%M%S')
+    Per_list = []
+    Tem_list = []
+    vmr_list = []
 
-    print(f'Processing am {amDate.strftime("%d %H:%M")} with Balloon {date.strftime("%d %H")}')
-    try:
-        df = WyomingUpperAir.request_data(date, station)        
-    except:
-        print(f'No data for {station} on {date}')
-        num_date += 1
-        continue    
+    #print(f'Processing am {amDate.strftime("%d %H:%M")} with Balloon {date.strftime("%d %H")}')   
     #skip if not the same day
-    if date.day < amDate.day:
-        print(f'Skipping {date} because it is not on the same day as {amDate}')
+    if date.month == amDate.month:
+        if date.day < amDate.day:
+            #print(f'Skipping Balloon {date} because it is not on the same day as {amDate}')
+            num_date += 1
+            continue
+        if date.day > amDate.day:
+            #print(f'Skipping am {amDate} because it is not on the same day as {date}')
+            num_file_n += 1
+            continue
+        if amDate.hour != 11:
+            #print(f'Skipping am {amDate} because it is not around 12:00 UTC')
+            num_file_n += 1
+            continue
+    elif date.month < amDate.month:
+        #print(f'Add Balloon {date} because less than {amDate}')
         num_date += 1
         continue
-    if date.day > amDate.day:
-        print(f'Skipping {amDate} because it is not on the same day as {date}')
+    elif date.month > amDate.month:
+        #print(f'Add am {amDate} because less than {date}')
         num_file_n += 1
         continue
     #compare with balloon data around 12:00 UTC WVR data around 11:50(the one before 12:00 UTC) 
-    if abs((date - amDate).total_seconds()) <= 600:            
+    if abs((date - amDate).total_seconds()) <= 700:            
+        try:
+            time.sleep(0.01)
+            df = WyomingUpperAir.request_data(date, station)        
+        except:
+            print(f'No data for {station} on {date}')
+            num_date += 1
+            continue 
         print(f'Comparing {date} with {amDate}')
         with open(file_path_n, 'r') as file:
             lines = file.readlines()
@@ -110,9 +126,10 @@ while num_date < len(data_list) and num_file_n < len(amc_list):
         Per = np.array(Per_list)
         Tem_C = np.array(Tem_C)
 
-        fig = plt.figure(figsize=(9, 9))
+        fig = plt.figure(figsize=(12, 12))
         skewt2 = SkewT(fig=fig, rotation=45)
         # plot sounding data
+
         skewt2.plot(Per, Tem_C, 'r', label='am Air Temperature') # air temperature
         skewt2.plot(Per, T_dry, 'b', label='am Dew Point') # dew point
         skewt2.plot(p, T, 'g', ls = '--', label='Balloon  Air Temperature') # air temperature
@@ -120,12 +137,13 @@ while num_date < len(data_list) and num_file_n < len(amc_list):
 
         skewt2.ax.set_xlabel('Temperature (°C)')
         skewt2.ax.set_ylabel('Pressure (hPa)')
-
-        skewt2.ax.legend(loc='best')
+        skewt2.ax.set_ylim(750, 10)
+        skewt2.ax.set_xlim(-50, 110)
+        skewt2.ax.legend(loc='best', fontsize=24)
         plt.title(f'Skew-T South Pole \n Balloon {date.strftime("%Y-%m-%d %H")} \n am {amDate.strftime("%Y-%m-%d %H:%M:%S")}', 
-                fontsize=16)
+                fontsize=32)
         fig.savefig(os.path.join(base_dir, f'fig1/SkewT_SP_{amDate.strftime("%Y-%m-%d %H")}.png'), dpi=300)
-        #plt.clf()
+        plt.clf()
         plt.close(fig)
 
         num_date += 1
